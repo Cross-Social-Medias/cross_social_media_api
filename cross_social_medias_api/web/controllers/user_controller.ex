3a0cee7e92e33b4d194fc6d2dae99bdfde4d08da
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-02-14 00:10:27 +0100

    create controller with static data

---
+++ cross_social_medias_api/web/controllers/user_controller.ex
@@ -0,0 +1,29 @@
+# web/controllers/user_controller
+defmodule CrossSocialMediasApi.UserController do
+  use CrossSocialMediasApi.Web, :controller
+
+def index(conn, _params) do
+    users = [
+      %ApiExample.User{ id: 1,
+                        name: "Joe",
+                        email: "joe@example.com",
+                        password: "topsecret",
+                        stooge: "moe"},
+
+      %ApiExample.User{ id: 2,
+                        name: "Anne",
+                        email: "anne@example.com",
+                        password: "guessme",
+                        stooge: "larry"},
+
+      %ApiExample.User{ id: 3,
+                        name: "Franklin",
+                        email: "franklin@example.com",
+                        password: "guessme",
+                        stooge: "curly"},
+
+    ]
+
+    json conn, users
+  end
+end
