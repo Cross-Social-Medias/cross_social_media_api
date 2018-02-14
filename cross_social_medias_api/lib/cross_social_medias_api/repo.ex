42561c3e9fea6514abb40934b9900a269abbcd4d
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-02-15 00:43:30 +0100

    move data in repo

--- cross_social_medias_api/lib/cross_social_medias_api/repo.ex
+++ cross_social_medias_api/lib/cross_social_medias_api/repo.ex
@@ -1,3 +1,27 @@
 defmodule CrossSocialMediasApi.Repo do
   use Ecto.Repo, otp_app: :cross_social_medias_api
+  
+	def users() do
+    [
+      %CrossSocialMediasApi.User{id: 1,
+                       name: "Joe",
+                       email: "joe@example.com",
+                       password: "topsecret",
+                       stooge: "moe"},
+
+      %CrossSocialMediasApi.User{id: 2,
+                       name: "Anne",
+                       email: "anne@example.com",
+                       password: "guessme",
+                       stooge: "larry"},
+
+      %CrossSocialMediasApi.User{id: 3,
+                       name: "Franklin",
+                       email: "franklin@example.com",
+                       password: "guessme",
+                       stooge: "curly"},
+
+    ]
+  end
+
 end
--- cross_social_medias_api/web/controllers/user_controller.ex
+++ cross_social_medias_api/web/controllers/user_controller.ex
@@ -2,28 +2,9 @@
 defmodule CrossSocialMediasApi.UserController do
   use CrossSocialMediasApi.Web, :controller
 
-def index(conn, _params) do
-    users = [
-      %ApiExample.User{ id: 1,
-                        name: "Joe",
-                        email: "joe@example.com",
-                        password: "topsecret",
-                        stooge: "moe"},
-
-      %ApiExample.User{ id: 2,
-                        name: "Anne",
-                        email: "anne@example.com",
-                        password: "guessme",
-                        stooge: "larry"},
-
-      %ApiExample.User{ id: 3,
-                        name: "Franklin",
-                        email: "franklin@example.com",
-                        password: "guessme",
-                        stooge: "curly"},
-
-    ]
-
+  def index(conn, _params) do
+    users = Repo.users()
     json conn, users
   end
+
 end
