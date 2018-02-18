6e8060e4458165287735550661baf7817b365254
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-02-19 00:17:30 +0100

    add two users

--- cross_social_medias_api/priv/repo/seeds.exs
+++ cross_social_medias_api/priv/repo/seeds.exs
@@ -9,3 +9,6 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will halt execution if something goes wrong.
+
+CrossSocialMediasApi.Repo.insert(%CrossSocialMediasApi.User{name: "Guillaume", email: "guillaume@domain.com", password: "secret", stooge: "tripleG"})
+CrossSocialMediasApi.Repo.insert(%CrossSocialMediasApi.User{name: "Bertrand", email: "bertrand@domain.com", password: "donttell", stooge: "ber"})
