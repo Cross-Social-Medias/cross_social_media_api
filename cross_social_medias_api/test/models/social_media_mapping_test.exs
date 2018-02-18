4d93c5791ddac973b57f0f2f4d129f25c36e6996
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-02-19 00:31:36 +0100

    add tests in models

---
+++ cross_social_medias_api/test/models/social_media_mapping_test.exs
@@ -0,0 +1,27 @@
+defmodule CrossSocialMediasApi.SocialMediaMappingTest do
+  use CrossSocialMediasApi.ModelCase
+
+  alias CrossSocialMediasApi.SocialMediaMapping
+    
+  @valid_attrs %{mapping_name: "Anthony Lastella", twitter_username: "AnthonyLastella", instagram_username: "anthonylastella"}
+  @invalid_attrs %{}
+
+  test "changeset with valid attributes" do
+    changeset = SocialMediaMapping.changeset(%SocialMediaMapping{}, @valid_attrs)
+    assert changeset.valid?
+  end
+
+  test "changeset with invalid attributes" do
+    changeset = SocialMediaMapping.changeset(%SocialMediaMapping{}, @invalid_attrs)
+    refute changeset.valid?
+  end
+
+  test "mapping_name, twitter_username are required" do
+    changeset = SocialMediaMapping.changeset(%SocialMediaMapping{}, Map.delete(@valid_attrs, :mapping_name))
+    refute changeset.valid?
+
+    changeset_2 = SocialMediaMapping.changeset(%SocialMediaMapping{}, Map.delete(@valid_attrs, :twitter_username))
+    refute changeset_2.valid?
+  end
+
+end
