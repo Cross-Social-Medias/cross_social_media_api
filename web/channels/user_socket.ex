5e0692ad7e9c193ae82275ad5d9aeb69825534b2
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-02-23 21:06:29 +0100

    try to fix deployment issue

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -5,7 +5,8 @@ defmodule CrossSocialMediasApi.UserSocket do
   # channel "room:*", CrossSocialMediasApi.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket, timeout: 45_000
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
