1ead2bd4756e4382136c6c84cc785fd631bf4de3
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-03-03 18:43:32 +0100

    add new field in user to know who created the mapping

---
+++ priv/repo/migrations/20180303173441_add_user_id_in_social_media_mapping.exs
@@ -0,0 +1,10 @@
+defmodule CrossSocialMediasApi.Repo.Migrations.AddUserIdInSocialMediaMapping do
+  use Ecto.Migration
+
+  def change do
+  	alter table(:social_media_mappings) do
+  		add :created_by, :integer 
+  	end
+
+  end
+end
