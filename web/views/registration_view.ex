84ab2630b2f163e2f34273be2fd64f0844e2eb03
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-03-08 22:44:44 +0100

    add registration views

---
+++ web/views/registration_view.ex
@@ -0,0 +1,20 @@
+defmodule CrossSocialMediasApi.RegistrationView do
+  use CrossSocialMediasApi.Web, :view
+
+  def render("success.json", %{user: user}) do
+    %{
+      status: :ok,
+      message: """
+        Now you can sign in using your email and password at /api/sign_in. You will receive JWT token.
+        Please put this token into Authorization header for all authorized requests.
+      """
+    }
+  end
+
+  def render("error.json", _changeset) do
+    %{ 
+      status: :unprocessable_entity,
+      message: "User cannot be created"
+    }
+  end
+end
