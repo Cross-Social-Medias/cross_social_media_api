ef97fbc37506a96b68df2b13cddfb6c0607cecd5
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-03-10 16:52:54 +0100

    remove commented code

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -24,23 +24,4 @@ defmodule CrossSocialMediasApi.SessionController do
   # respond_somehow(conn)
   # end 
 
-  # def sign_in(conn, %{"session" => %{"email" => email, "password" => password}}) do  
-  #   case User.find_and_confirm_password(email, password) do
-  #     {:ok, user} ->
-  #       new_conn = Guardian.Plug.api_sign_in(conn, user)
-  #       jwt = Guardian.Plug.current_token(new_conn)
-  #       claims = Guardian.Plug.claims(new_conn)
-  #       exp = Map.get(claims, "exp")
-
-  #       new_conn
-  #       |> put_resp_header("authorization", "Bearer #{jwt}")
-  #       |> put_resp_header("x-expires", exp)
-  #       |> render("sign_in.json", user: user, jwt: jwt, exp: exp)
-  #     {:error, _reason} ->
-  #       conn
-  #       |> put_status(401)
-  #       |> render("error.json", message: "Could not login")
-  #   end
-  # end
-
 end
