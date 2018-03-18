13df9cef9840af19aa926739a9f3fb0d08e55be2
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-03-19 00:35:49 +0100

    update csm view on index and search

--- web/views/social_media_mapping_view.ex
+++ web/views/social_media_mapping_view.ex
@@ -2,7 +2,7 @@ defmodule CrossSocialMediasApi.SocialMediaMappingView do
   use CrossSocialMediasApi.Web, :view
 
   def render("index.json", %{social_media_mappings: social_media_mappings}) do
-    %{data: render_many(social_media_mappings, CrossSocialMediasApi.SocialMediaMappingView, "social_media_mapping.json")}
+    %{mappings: render_many(social_media_mappings, CrossSocialMediasApi.SocialMediaMappingView, "social_media_mapping.json")}
   end
 
   def render("social_media_mapping.json", %{social_media_mapping: social_media_mapping}) do
