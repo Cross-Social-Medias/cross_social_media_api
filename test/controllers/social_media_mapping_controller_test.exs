a63ea7c26b12c19b79393e6bf97bbfcece6b3f1f
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-03-19 00:16:35 +0100

    add new test

--- test/controllers/social_media_mapping_controller_test.exs
+++ test/controllers/social_media_mapping_controller_test.exs
@@ -107,4 +107,61 @@ defmodule CrossSocialMediasApi.SocialMediaMappingControllerTest do
     end
   end
 
+  describe "search/2" do
+    test "search a mapping name, should return two records", %{user: user, jwt: jwt} do
+      social_media_mappings = [
+        SocialMediaMapping.changeset(%SocialMediaMapping{}, %{mapping_name: "Anthony Lastella", twitter_username: "AnthonyLastella", instagram_username: "anthonyLastella", youtube_channel_id: "123465", created_by: user.id, user_id: user.id}),
+        SocialMediaMapping.changeset(%SocialMediaMapping{}, %{mapping_name: "John doe", twitter_username: "johnDoe", instagram_username: "JoJo", youtube_channel_id: "123465", created_by: user.id, user_id: user.id}),
+        SocialMediaMapping.changeset(%SocialMediaMapping{}, %{mapping_name: "super John", twitter_username: "john", instagram_username: "Jo", youtube_channel_id: "123465", created_by: user.id, user_id: user.id}),
+        SocialMediaMapping.changeset(%SocialMediaMapping{}, %{mapping_name: "Bar", twitter_username: "Bar", instagram_username: "Bar", youtube_channel_id: "123465", created_by: user.id, user_id: user.id}),
+        SocialMediaMapping.changeset(%SocialMediaMapping{}, %{mapping_name: "Foo", twitter_username: "Foo", instagram_username: "Foo", youtube_channel_id: "123465", created_by: user.id, user_id: user.id})
+      ]
+      Enum.each(social_media_mappings, &Repo.insert!(&1))
+
+      response = build_conn()
+        |> put_req_header("authorization", "Bearer #{jwt}")
+        |> get(social_media_mapping_path(build_conn(), :search, mapping_name: "John"))
+        |> json_response(200)
+
+      expected = %{
+        "data" => [
+          %{
+            "created_by" => user.id,
+            "instagram_username" => "JoJo",
+            "mapping_name" => "John doe",
+            "twitter_username" => "johnDoe",
+            "youtube_channel_id" => "123465"
+          },
+          %{
+            "created_by" => user.id,
+            "instagram_username" => "Jo",
+            "mapping_name" => "super John",
+            "twitter_username" => "john",
+            "youtube_channel_id" => "123465"
+          }
+        ]
+      }
+      assert response == expected
+    end
+
+    test "search a mapping, should return an empty array", %{user: user, jwt: jwt} do
+      social_media_mappings = [
+        SocialMediaMapping.changeset(%SocialMediaMapping{}, %{mapping_name: "Anthony Lastella", twitter_username: "AnthonyLastella", instagram_username: "anthonyLastella", youtube_channel_id: "123465", created_by: user.id, user_id: user.id}),
+        SocialMediaMapping.changeset(%SocialMediaMapping{}, %{mapping_name: "John doe", twitter_username: "johnDoe", instagram_username: "JoJo", youtube_channel_id: "123465", created_by: user.id, user_id: user.id}),
+        SocialMediaMapping.changeset(%SocialMediaMapping{}, %{mapping_name: "super John", twitter_username: "john", instagram_username: "Jo", youtube_channel_id: "123465", created_by: user.id, user_id: user.id}),
+        SocialMediaMapping.changeset(%SocialMediaMapping{}, %{mapping_name: "Bar", twitter_username: "Bar", instagram_username: "Bar", youtube_channel_id: "123465", created_by: user.id, user_id: user.id}),
+        SocialMediaMapping.changeset(%SocialMediaMapping{}, %{mapping_name: "Foo", twitter_username: "Foo", instagram_username: "Foo", youtube_channel_id: "123465", created_by: user.id, user_id: user.id})
+      ]
+      Enum.each(social_media_mappings, &Repo.insert!(&1))
+
+      response = build_conn()
+        |> put_req_header("authorization", "Bearer #{jwt}")
+        |> get(social_media_mapping_path(build_conn(), :search, mapping_name: "Wrong research"))
+        |> json_response(200)
+
+      expected = %{ "data" => []}
+      assert response == expected
+    end
+  end
+
 end
